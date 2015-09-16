bc4cf33ce34ea8befb70f078ed4dc356c8777857
tivaliy <vitaliy@kulanov.org.ua>
2015-09-16 09:32:00 +0300

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -9,22 +9,6 @@ from tests import TESTS
 
 cover = """def cover(f, data):
     return f(list(tuple(d) for d in data))"""
-# cover = """def cover(func, data):
-#     cdata = list(tuple(d) for d in data)
-#     res = func(cdata)
-#     if not isinstance(res, list):
-#         raise TypeError("Must be a list.")
-#     return res, str(res)
-# """
-
-
-def checker(data, user_data):
-    # user_result, str_result = user_data
-    for t in user_data:
-        if not isinstance(t, tuple):
-            return False, (False, "You should return a list of tuples.")
-    return True, (True, "Great")
-
 
 api.add_listener(
     ON_CONNECT,
@@ -34,5 +18,4 @@ api.add_listener(
             'python-27': cover,
             'python-3': cover
         },
-        checker=checker,
     ).on_ready)
