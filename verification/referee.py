eeb7655e9d2bd0b51e59537d6469ea54f84b4ba1
tivaliy <vitaliy@kulanov.org.ua>
2015-10-21 14:01:44 +0300

    add custom checker

--- verification/referee.py
+++ verification/referee.py
@@ -11,26 +11,18 @@ cover = """def cover(f, data):
     return f(list(tuple(d) for d in data))"""
 
 
-# def checker(user_answer, right_answer):
-#     if not isinstance(user_answer, (list, tuple)) or \
-#              any(not isinstance(line, (list, tuple)) for line in user_answer):
-#         return False, "It's not lists"
-#     if any(not isinstance(x, (float, int)) for line in user_answer for x in line):
-#         return False, "It's not numbers"
-#     precision = 0.01
-#     return all(abs(right_coord - user_coord) <= precision
-#              for right_line, user_line in zip(right_answer, user_answer)
-#                for right_coord, user_coord in zip(right_line, user_line)), None
-#
-# api.add_listener(
-#     ON_CONNECT,
-#     CheckiOReferee(
-#         tests=TESTS,
-#         cover_code={
-#             'python-27': cover,
-#             'python-3': cover
-#         },
-#     checker=checker).on_ready)
+def checker(right_answer, user_answer):
+    if not user_answer:
+        return False, "Your answer is empty"
+    if not isinstance(user_answer, (list, tuple)) or \
+             any(not isinstance(line, (list, tuple)) for line in user_answer):
+        return False, "It's not lists"
+    if any(not isinstance(x, (float, int)) for line in user_answer for x in line):
+        return False, "It's not numbers"
+    precision = 0.01
+    return all(abs(right_coord - user_coord) <= precision
+             for right_line, user_line in zip(right_answer, user_answer)
+               for right_coord, user_coord in zip(right_line, user_line)), None
 
 api.add_listener(
     ON_CONNECT,
@@ -40,4 +32,14 @@ api.add_listener(
             'python-27': cover,
             'python-3': cover
         },
-    ).on_ready)
+    checker=checker).on_ready)
+
+# api.add_listener(
+#     ON_CONNECT,
+#     CheckiOReferee(
+#         tests=TESTS,
+#         cover_code={
+#             'python-27': cover,
+#             'python-3': cover
+#         },
+#     ).on_ready)
